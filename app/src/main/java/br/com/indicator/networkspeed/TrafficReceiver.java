7b557e073d08a8effe8d5e8da290617af249d47c
Kessiler Rodrigues <keessiler@hotmail.com>
2015-04-12 17:17:26 -0300

    Revert to 6c471c0517d8b67d63cded055ab8cc2a019a10a7

--- app/src/main/java/br/com/indicator/networkspeed/TrafficReceiver.java
+++ app/src/main/java/br/com/indicator/networkspeed/TrafficReceiver.java
@@ -2,9 +2,11 @@ package br.com.indicator.networkspeed;
 
 import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.util.Log;
 
 
 public class TrafficReceiver extends BroadcastReceiver {
@@ -12,6 +14,11 @@ public class TrafficReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            Log.d("traffic", "send a broadcast");
+            final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName trafficWidget = new ComponentName(context, TrafficWidget.class);
+            int[] widgetIds = appWidgetManager.getAppWidgetIds(trafficWidget);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, widgetIds);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             context.sendBroadcast(intent);
         }
--- app/src/main/java/br/com/indicator/networkspeed/TrafficWidget.java
+++ app/src/main/java/br/com/indicator/networkspeed/TrafficWidget.java
@@ -5,44 +5,48 @@ import android.appwidget.AppWidgetProvider;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
+import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 public class TrafficWidget extends AppWidgetProvider {
-
     private boolean mAttached;
-    private BroadcastReceiver mIntentReceiver;
     private TrafficSpeed mTrafficSpeed;
+    private BroadcastReceiver mIntentReceiver;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
-        if (!mAttached) {
-            mAttached = true;
-            mIntentReceiver = new TrafficReceiver();
-            mTrafficSpeed = new TrafficSpeed(context);
-            IntentFilter filter = new IntentFilter();
-            filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
-            context.registerReceiver(mIntentReceiver, filter);
-        } else {
-            if (mTrafficSpeed.isNetworkConnected()) {
-                String textSpeed = "Download: " + mTrafficSpeed.getFormattedDownSpeed() + " / Upload: " + mTrafficSpeed.getFormattedUpSpeed();
-                RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.traffic_widget);
-                remoteViews.setTextViewText(R.id.textView, textSpeed);
-                ComponentName trafficComponent = new ComponentName(context, TrafficWidget.class);
-                appWidgetManager.updateAppWidget(trafficComponent, remoteViews);
-            }
+        Resources resources = context.getResources();
+        String textSpeed = resources.getString(R.string.waitingConnection);
+        if (mTrafficSpeed.isNetworkConnected()) {
+            textSpeed = resources.getString(R.string.download) + ": " + mTrafficSpeed.getFormattedDownSpeed() + " / " +
+                    resources.getString(R.string.upload) + ": " + mTrafficSpeed.getFormattedUpSpeed();
         }
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.traffic_widget);
+        remoteViews.setTextViewText(R.id.textView, textSpeed);
+        ComponentName trafficComponent = new ComponentName(context, TrafficWidget.class);
+        appWidgetManager.updateAppWidget(trafficComponent, remoteViews);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        Log.d("traffic", "onEnabled - teste");
+        mTrafficSpeed = new TrafficSpeed(context);
+        mIntentReceiver = new TrafficReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        context.getApplicationContext().registerReceiver(mIntentReceiver, filter);
+
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
-        if (mAttached) {
-            context.unregisterReceiver(mIntentReceiver);
-            mAttached = false;
-        }
+        Log.d("traffic", "onDeleted - teste");
+        context.getApplicationContext().unregisterReceiver(mIntentReceiver);
     }
 
 }
